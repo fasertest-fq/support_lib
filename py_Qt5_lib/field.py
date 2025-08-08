22940f47015739e7ae57efd41a8f5c7d5d903a85
fasertest-fq <gresh1kone@gmail.com>
2025-08-09 00:16:25 +0500

    for pyqt5 template

---
+++ py_Qt5_lib/field.py
@@ -0,0 +1,62 @@
+#support templates for easy create pyqt5 gui interface.
+
+import PyQt5.QtWidgets as qw
+import PyQt5.QtGui as qgi
+import PyQt5.QtCore as qc
+
+
+class InputField(qw.QWidget):
+    """
+    textline with description for input information.
+    """
+
+    def __init__(self, name:str):
+        super().__init__()
+        self.label = qw.QLabel()
+        self.label.setText(name)
+        self.input_f=qw.QLineEdit()
+        
+        
+        self.layout = qw.QHBoxLayout()
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.input_f)
+        
+        self.setLayout(self.layout)
+        
+    def return_info(self):
+        return {self.label.text(): self.input_f.text()}
+        
+class WLine(qw.QWidget):
+    """
+    multiple InputFields
+    """
+
+    def __init__(self, name_list:list):
+        super().__init__()
+        self.name_list=name_list
+        
+        self.layout=qw.QHBoxLayout()
+        self.setLayout(self.layout)
+        
+        self.draw_Widgets()
+        
+    def draw_Widgets(self):
+        for name in self.name_list:
+            self.layout.addWidget(InputField(name))
+            
+    def get_info(self):
+        #for get information by textline
+        widget_arr=self.findChildren(InputField)
+        return_dict={}
+        for widget_one in widget_arr:
+            try:
+                temp_dict=widget_one.return_info()
+                return_dict.update(temp_dict)
+            except:
+                pass
+        return return_dict
+
+
+
+        
+#©created by fasertest-fq in 07.08.2025 23:24
