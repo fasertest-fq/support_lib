d5f331a5279eb9144eddd59c0d084d9a33d178b6
fasertest-fq <gresh1kone@gmail.com>
2025-08-24 19:09:29 +0500

    add templates for pyQt

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # support_lib
-support lib for work
+
+support lib for work with another projects.
+
+## Description
+
+<ul>
+<li><b>py_Qt5_lib</b> - templates for work with pyQt5. </li>
+<li><b>py_Qt6_lib</b> - templates for work with pyQt6. </li>
+</ul>
---
+++ py_Qt5_lib/warning_message.py
@@ -0,0 +1,22 @@
+import PyQt5.QtWidgets as qw
+
+class Warning_Window(qw.QWidget):
+    def __init__(self, e):
+        super().__init__()
+        self.setGeometry(300, 300, 400, 200)
+        self.setWindowTitle('Warning')
+        self._label = qw.QLabel()
+        self._label.setText(e)
+        
+        self._button = qw.QPushButton(' Ok ', self)
+        self._button.clicked.connect(self.close)
+        
+        self._layout = qw.QVBoxLayout()
+        self._layout.addWidget(self._label)
+        self._layout.addWidget(self._button)
+        
+        self.setLayout(self._layout)
+        
+        
+        
+#©created by fasertest-fq in 24.08.2025 18:56
